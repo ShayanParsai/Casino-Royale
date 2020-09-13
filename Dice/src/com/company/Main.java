3fdf770a61ca8c11d5231fc012f0424db70995fc
Shayan Parsai <70860248+ShayanParsai@users.noreply.github.com>
2020-09-13 20:46:50 +0200

    Dice V3

--- Dice/src/com/company/Main.java
+++ Dice/src/com/company/Main.java
@@ -7,19 +7,17 @@ import java.time.LocalDateTime;
 
 public class Main {
 
-    static double D6(double min, double max){
-        double x = (int)(Math.random()*((6-1))+min);
-        return x;
+    static double D6() {
+        return (int)(Math.random()*((6-1))+ (double) 1);
     }
-    static double D12(double min, double max){
-        double x = (int)(Math.random()*((12-1))+min);
-        return x;
+    static double D12() {
+        return (int)(Math.random()*((12-1))+ (double) 1);
     }
 
     public static void main(String[] args) {
 
-        ArrayList<Double> SaveD6 = new ArrayList<Double>();
-        ArrayList<Double> SaveD12 = new ArrayList<Double>();
+        ArrayList<Double> SaveD6 = new ArrayList<>();
+        ArrayList<Double> SaveD12 = new ArrayList<>();
 
         DateTimeFormatter DATE = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 
@@ -33,9 +31,8 @@ public class Main {
         String NamN = Namn.next();
 
         int loop = 1;
-        Double sum = 0.0;
         System.out.println("========================");
-        System.out.println("Welcome "+NamN+" to Shayans program!");
+        System.out.println("Welcome "+NamN+" to the Dicer!");
 
         while (loop == 1) {
 
@@ -47,50 +44,40 @@ public class Main {
 
             Scanner Menyval = new Scanner(System.in);
             System.out.print("Menyval: ");
-            int i = Menyval.nextInt();
+            int x = Menyval.nextInt();
 
-            switch (i){
-
-                case 1:  // Rullar tärning med 6 sidor, sparar tärnings rull till ArrayList
-
+            switch (x) {
+                case 1 -> { // Rullar tärning med 6 sidor, sparar tärnings rull till ArrayList SaveD6
                     System.out.println("========================");
-                    Double Roll1 = D6(1,6);
+                    Double Roll1 = D6();
                     SaveD6.add(Roll1);
-                    System.out.println("You rolled "+ Roll1);
-                    break;
-
-                case 2: // Rullar tärning med 12 sidor, sparar tärnings rull till ArrayList
-
+                    System.out.println("You rolled " + Roll1);
+                }
+                case 2 -> { // Rullar tärning med 12 sidor, sparar tärnings rull till ArrayList SaveD12
                     System.out.println("========================");
-                    Double Roll2 = D12(1,12);
+                    Double Roll2 = D12();
                     SaveD12.add(Roll2);
-                    System.out.println("You rolled "+ Roll2);
-                    break;
-
-                case 3: // Sorterar båda ArrayList, skriver ut alla sparade tärnings rull i ordning
-
+                    System.out.println("You rolled " + Roll2);
+                }
+                case 3 -> { // Sorterar båda listorna, skriver ut alla sparade resultat i storleks ordning
                     System.out.println("========================");
                     Collections.sort(SaveD6);
                     Collections.sort(SaveD12);
                     System.out.println("All your previous D6 rolls: " + SaveD6);
                     System.out.println("All your previous D12 rolls: " + SaveD12);
-                    break;
-
-                case 4: // Exit
-
+                }
+                case 4 -> { // Exit
                     System.out.println("========================");
                     LocalDateTime NOW1 = LocalDateTime.now();
                     System.out.println(DATE.format(NOW1));
                     System.out.println("========================");
-                    System.out.println("Thank you "+NamN+" for using my program");
+                    System.out.println("Thank you " + NamN + " for using my program");
                     loop++;
-                    break;
-
-                default: // Om användaren skriver in ett menyval som inte finns
-
+                }
+                default -> { // Om användaren skriver in ett menyval som inte finns
                     System.out.println("========================");
                     System.out.println("You can only choose between 1-4");
-                    break;
+                }
             }
         }
     }
